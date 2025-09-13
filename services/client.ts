2256024042a8cf8a0172b316f6d19941bc6571ed
nguyentanphat <nguyentanphattt0907@gmail.com>
2025-09-13 18:51:16 +0700

    fix: apk 6

--- services/client.ts
+++ services/client.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 import { getNewToken } from "./user";
 
-const BASE_URL = "http://168.138.168.177:25565/";
+//const BASE_URL = "http://168.138.168.177:25565/";
+const BASE_URL = "https://168.138.168.177:25566/";
 
 export const publicClient = axios.create({
   baseURL: BASE_URL,
