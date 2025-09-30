144575380c3735b7f4397f8f4a305e163bf1e427
nguyentanphat <nguyentanphattt0907@gmail.com>
2025-09-30 13:18:51 +0700

    feat: blog page

--- app/(tabs)/news.tsx
+++ app/(tabs)/news.tsx
@@ -1,8 +1,123 @@
-import React from 'react'
+import { images } from '@/constants/image';
+import { FontAwesome6 } from '@expo/vector-icons';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { FlatList, Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 
 const News = () => {
+  const { t } = useTranslation();
+  const [selectedTag, setSelectedTag] = useState("all");
+  const options = [
+    {
+      id: 1,
+      label: "Tất cả",
+      value: "all",
+    },
+    {
+      id: 2,
+      label: "Nước",
+      value: "water",
+    },
+    {
+      id: 3,
+      label: "Thức ăn",
+      value: "food",
+    },
+
+    {
+      id: 4,
+      label: "Vận động",
+      value: "fitness",
+    },
+
+    {
+      id: 5,
+      label: "Giấc ngủ",
+      value: "sleep",
+    },
+
+    {
+      id: 6,
+      label: "Khác",
+      value: "other",
+    },
+  ]
   return (
-    <div>News</div>
+    <View className="flex-1 relative">
+      <ScrollView
+        className="flex-1 gap-2.5 px-4 pb-10 font-lato-regular bg-[#f6f6f6]"
+        stickyHeaderIndices={[0]}
+        contentContainerStyle={{ paddingBottom: 50 }}
+        showsVerticalScrollIndicator={false}
+      >
+      <View className="flex bg-[#f6f6f6] pt-16">
+        <Text className="text-2xl font-bold  self-center">{t("Tin tức")}</Text>
+      </View>
+
+      <View className='flex flex-row items-center justify-between'>
+        <View>
+          <Text className="font-bold text-xl pt-5 pb-2">{t("Loại")}</Text>
+          <View className="flex-row gap-3">
+            <FlatList
+              data={options}
+              keyExtractor={(item) => item?.id?.toString() ?? ""}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{ gap: 12 }}
+              renderItem={({ item }) => {
+                const isSelectedTag = selectedTag === item?.value;
+                return (
+                  <TouchableOpacity
+                    onPress={() => setSelectedTag(item?.value ?? "")}
+                    className={`px-4 py-2 rounded-full ${isSelectedTag ? "bg-cyan-blue" : "bg-white"}`}
+                    style={{ marginRight: 0 }}
+                  >
+                    <Text
+                      className={`text-lg ${isSelectedTag ? "text-white" : "text-black"}`}
+                    >
+                      {item?.label}
+                    </Text>
+                  </TouchableOpacity>
+                );
+              }}
+            />
+          </View>
+        </View>
+      </View>
+
+      <View className=' py-2.5'>
+        <View className='relative bg-white rounded-md shadow-md flex justify-between gap-2 w-full px-4 py-4'>
+          <Image source={images.food01} className='w-full h-[150px] rounded-lg' width={300} height={250} />
+          <View className='absolute top-4 right-4 flex items-center justify-center bg-cyan-blue rounded-full px-4 py-2'>
+            <Text className='text-white'>Fitness</Text>
+          </View>
+          <Text className='text-center text-3xl font-bold'>A day with fitness</Text>
+
+          <View className='flex flex-row items-center justify-center gap-2'>
+            <Text className='text-black/60 text-lg'>Hieu Phan Trung</Text>
+            <Text className='text-black/60 text-lg'>20/09/2025 12:00</Text>
+          </View>
+          <View className='border-t border-black/10 pt-4'>
+            <Text
+              numberOfLines={2}
+              ellipsizeMode="tail"
+              className="text-black/70 text-base leading-5"
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            </Text>
+            <TouchableOpacity className="mt-2">
+              <Text className="text-cyan-blue font-semibold">Xem thêm</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+
+      </ScrollView>
+      
+      <TouchableOpacity className='absolute bottom-4 right-4 w-[60px] h-[60px] flex items-center justify-center bg-cyan-blue rounded-full shadow-lg'>
+        <FontAwesome6 name="plus" size={24} color="white" />
+      </TouchableOpacity>
+    </View>
   )
 }
 
